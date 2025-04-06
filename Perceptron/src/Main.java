3b3a545771fd42fdcd1001f835f2392580c0f5a8
Dmytro Brahinskyi <dbraginskiy821@gmail.com>
2025-04-06 17:01:51 +0200

    Working Perceptron commit

--- Perceptron/src/Main.java
+++ Perceptron/src/Main.java
@@ -1,10 +1,32 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+    private static List<Point> trainPoints;
+    private static List<Point> testPoints;
+
     public static void main(String[] args) {
-        String trainPath = "C:\\Users\\Dimka\\IdeaProjects\\NAI\\Perceptron\\src\\train\\";
         Scanner scanner = new Scanner(System.in);
+
+        filesLoading(scanner);
+
+        System.out.println("Pick a learning rate:");
+        double learnRate = scanner.nextDouble();
+
+        System.out.print("Enter number of epochs: ");
+        int epochs = scanner.nextInt();
+
+        Perceptron perceptron = new Perceptron(trainPoints.getFirst().getVector().length, learnRate);
+        perceptron.train(trainPoints, epochs);
+
+        System.out.println("Train Accuracy: " + (perceptron.calculateAccuracy(trainPoints) * 100) + "%");
+        System.out.println("Test Accuracy: " + (perceptron.calculateAccuracy(testPoints) * 100) + "%");
+    }
+
+    private static void filesLoading(Scanner scanner){
+        String trainPath = "C:\\Users\\Dimka\\IdeaProjects\\NAI\\Perceptron\\src\\train\\";
 
         System.out.println("Input the train set name:");
         String trainSetName = scanner.nextLine();
@@ -20,8 +42,8 @@ public class Main {
         testPath+=testSetName;
 
 
-        List<Point> trainPoints =  DataLoader.load(trainPath);
-        List<Point> testPoints =  DataLoader.load(testPath);
+        trainPoints =  DataLoader.load(trainPath);
+        testPoints =  DataLoader.load(testPath);
 
 
         System.out.println("Your train set:");
@@ -33,7 +55,5 @@ public class Main {
         for(Point point : testPoints){
             System.out.println(point);
         }
-
-
     }
 }
--- Perceptron/src/Perceptron.java
+++ Perceptron/src/Perceptron.java
@@ -1,25 +1,130 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Perceptron {
 
-    public double netCalculator(double[] input, double[] weights, double bias) {
-        if(input.length != weights.length) {
+    private double[] weights;
+    private double bias;
+    private double learnRate;
+
+    private int epochs;
+    private double errorThreshold;
+
+    private Random random = new Random();
+
+    public Perceptron(int inputSize, double learnRate) {
+        this.weights = new double[inputSize];
+        this.learnRate = learnRate;
+
+        for(int i = 0; i < weights.length; i++) {
+            weights[i] = random.nextDouble() * 0.1;
+        }
+
+        this.bias = random.nextDouble();
+
+        System.out.println("Generated weights: " + Arrays.toString(weights) +
+                "\nGenerated bias: " + bias);
+    }
+
+    public void train(List<Point> trainSet, int epochs){ //updates Weights
+        for(int epoch = 0; epoch < epochs; epoch++){
+            int missclassified = 0;
+
+            for(Point point : trainSet){
+                double[] x = point.getVector();
+                int predicted = predict(point.getVector());
+                int expected = encodeLabel(point.getName());
+
+                if(predicted != expected){
+                    System.out.println(predicted + " != " + expected + ". Updating weights:");
+                    updateWeights(point.getVector(), expected, predicted);
+                    missclassified++;
+                }
+            }
+
+            if(missclassified == 0){
+                System.out.println("Converged at epoch: " + epoch);
+                break;
+            }
+        }
+    }
+
+    public int encodeLabel(String label){
+        return label.equals("Iris-versicolor") ? 0 : 1;
+    }
+
+    public String decodeLabel(int val){
+        return val == 0 ? "Iris-Versicolor" : "Iris-virginica";
+    }
+
+    public int predict(double[] input) { // Activation function
+        if(input == null || input.length != weights.length) {
             System.out.println("Error: input length != weights.length");
         }
 
         double net = 0.0;
+
         for(int i = 0; i < input.length; i++) {
             net += input[i] * weights[i];
         }
+
         net -= bias;
-        return bias;
+
+        int af = (net >= 0) ? 1 : 0;
+        //System.out.println("net: " + net + ". Activation function: " + af);
+
+        return (net >= 0) ? 1 : 0; // Activation function
     }
 
-    public int activationFunction(double net){
-        if(net >= 0){
-            return 1;
-        }else{
-            return 0;
+    public double updateIterError(List<Point> points){
+        double totalError = 0.0;
+        int n = points.size();
+
+        for(Point point : points){
+            int predicted = predict(point.getVector());
+            int expected = encodeLabel(point.getName());
+
+            double subError = expected - predicted;
+
+            totalError += Math.pow(subError, 2);
+
         }
+        return totalError / n;
     }
+
+    public void updateWeights(double[] input, int expected, int predicted){
+        double error = expected - predicted;
+
+        for(int i = 0; i < weights.length; i++){
+            weights[i] = weights[i] + learnRate * error * input[i];
+        }
+
+        bias = bias - learnRate*error;
+
+        System.out.println("Updated Weights: " + Arrays.toString(weights));
+    }
+
+    public double calculateAccuracy(List<Point> points){
+        int correct = 0;
+        int incorrect = 0;
+
+        for(Point point : points){
+            int predicted = predict(point.getVector());
+            int actual = encodeLabel(point.getName());
+
+            if(predicted == actual){
+                correct++;
+                System.out.println("Correct Prediction:\nPrediсted: " + predicted + " = Actual: " + actual);
+            }else{
+                System.out.println("\n!Incorrect Prediction:\nPrediсted: " + predicted + " != Actual: " + actual +"!\n");
+            }
+        }
+
+        return (double) correct / points.size(); // returning accuracy
+    }
+
+
+
+
 }
